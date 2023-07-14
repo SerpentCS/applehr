fa1df8ee82049470d3a7eb37f6e5fda553f1a436
Bhavesh Patil <bhaveshpatil4996@gmail.com>
2023-07-14 11:08:41 +0530

    [ADD] sync_clients_data_master : Functionality for integration from v16 to v10.

--- sync_clients_data_master/models/balance_history.py
+++ sync_clients_data_master/models/balance_history.py
@@ -1,4 +1,14 @@
 from odoo import api, fields, models
+import calendar
+import logging
+from datetime import datetime
+from itertools import groupby
+try:
+    from xmlrpc import client as xmlrpclib
+except ImportError:
+    import xmlrpclib
+
+_logger = logging.getLogger(__name__)
 
 
 class BalanceHistory(models.Model):
@@ -10,6 +20,7 @@ class BalanceHistory(models.Model):
     def _compute_balance(self):
         for line in self:
             line.balance = line.credit - line.debit
+            line.customer_balance()
 
     partner_id = fields.Many2one("res.partner", string="Customer")
     description = fields.Text(string="Description")
@@ -26,3 +37,36 @@ class BalanceHistory(models.Model):
         compute="_compute_balance", currency_field="company_currency_id",
         store=True, string='Balance')
 
+    def customer_balance(self):
+        partner = self.partner_id
+        for server in partner.remote_servers_ids:
+            user_balance_partner = self.partner_id.balance
+
+            if server:
+                addr = server.url
+                userid = server.user
+                password = server.password
+                dbname = server.dbname
+                try:
+                    common_2 = xmlrpclib.ServerProxy(
+                        '{}/xmlrpc/2/common'.format(addr))
+                    model_2 = xmlrpclib.ServerProxy(
+                        '{}/xmlrpc/2/object'.format(addr))
+                    odoo_10 = common_2.authenticate(
+                        dbname, userid, password, {})
+                except Exception as e:
+                    _logger.warning(
+                        "Could not authenticate user on the remote server: %s", e)
+                    continue
+                try:
+                    logs_data = model_2.execute_kw(dbname, odoo_10, password, 'customer.balance', 'search_read',
+                                                   [[('name', '=', 'customer_balance')]], {'fields': ["balance"]})
+
+                    for line in logs_data:
+                        new_data = model_2.execute_kw(dbname, odoo_10, password, 'customer.balance', 'write',
+                                                      [line['id'], {'balance': user_balance_partner}])
+                except Exception as e:
+                    _logger.warning(
+                        "Could not retrieve or update data from the client server: %s", e)
+                    continue
+        return True
