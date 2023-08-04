25e67cb910d71f9fd91ee7c88c6b1a681c03d103
deepak-scs <dipak.b.serpentcs@gmail.com>
2023-08-04 17:40:40 +0530

    [ADD] sync_clients_data_master: add new cron job fetch employee past date data

--- sync_clients_data_master/models/remote_server.py
+++ sync_clients_data_master/models/remote_server.py
@@ -33,135 +33,6 @@ class RemoteServer(models.Model):
     rate = fields.Float()
     start_date = fields.Date(string="Start Date")
 
-    def _fetch_client_log_data(self, server):
-        log_datas = []
-        if server:
-            addr = server.url
-            userid = server.user
-            password = server.password
-            dbname = server.dbname
-        try:
-            uid = xmlrpclib.ServerProxy("%s/xmlrpc/common" % (addr)).authenticate(
-                dbname, userid, password, {}
-            )
-        except:
-            _logger.warning("Could not authenticate user on the remote server")
-        try:
-            log_datas = xmlrpclib.ServerProxy("%s/xmlrpc/object" % (addr)).execute(
-                dbname,
-                uid,
-                password,
-                "partner.work.history",
-                "search_read",
-                ([
-                    ('employee_id', '!=', False),
-                    # ('join_date', '<=', self.start_date),
-                    '|',
-                    ('end_date', '>=', fields.Date.today()),
-                    ('end_date', '=', False)
-                ]),
-                ["partner_company_id", "employee_id", "emp_code"],
-            )
-        except:
-            _logger.warning("Could not retrieve data from client server")
-        employee_data = []
-        for logdata in log_datas:
-            vals = {
-                "partner_id": self.partner_id.id,
-                "emp_name": logdata.get("employee_id")[1],
-                "res_model": "hr.employee",
-                "emp_code": logdata.get("emp_code"),
-                "partner_company": logdata.get("partner_company_id")[1]
-                if logdata.get("partner_company_id")
-                else "",
-                "date": fields.Date.today(),
-                "remote_server_id": self.id,
-            }
-            client_data_rec = self.env["client.data"].search(
-                [
-                    ("date", "=", fields.Date.today()),
-                    ("emp_code", "=", logdata.get("emp_code")),
-                    ("remote_server_id", "=", self.id),
-                ],
-                limit=1,
-            )
-            if not client_data_rec:
-                employee_data.append(vals)
-        self.env["client.data"].create(employee_data)
-        server["date_sync"] = datetime.now()
-        return True
-
-    # def _fetch_client_past_date_deta(self, server):
-    #     log_datas = []
-    #     employee_data_dic = []
-    #     if server:
-    #         addr = server.url
-    #         userid = server.user
-    #         password = server.password
-    #         dbname = server.dbname
-    #     try:
-    #         uid = xmlrpclib.ServerProxy("%s/xmlrpc/common" % (addr)).authenticate(
-    #             dbname, userid, password, {}
-    #         )
-    #     except:
-    #         _logger.warning("Could not authenticate user on the remote server")
-    #     try:
-    #         log_datas = xmlrpclib.ServerProxy("%s/xmlrpc/object" % (addr)).execute(
-    #             dbname,
-    #             uid,
-    #             password,
-    #             "partner.work.history",
-    #             "search_read",
-    #             ([
-    #                 ('employee_id', '!=', False),
-    #                 ('join_date', '<=', self.start_date),
-    #                 ('write_date', '<=', fields.Date.today()),
-    #                 ('write_date', '>=', fields.Date.today()),
-    #                 '|',
-    #                 ('end_date', '>=', fields.Date.today()),
-    #                 ('end_date', '=', False)
-    #             ]),
-    #             ["partner_company_id", "employee_id", "emp_code"],
-    #         )
-    #         if self.start_date <= fields.Date.today():
-    #             for log_data in log_datas:
-    #                 number_of_days = 1
-    #                 differece_days = fields.Date.today() - self.start_date
-    #                 for day in range(differece_days.days + 1):
-    #                     date = self.start_date + relativedelta(day=number_of_days)
-    #                     employee_data_dic.append({
-    #                         'partner_company': log_data.get("partner_company_id")[1]
-    #                         if log_data.get("partner_company_id")
-    #                         else "",
-    #                         "emp_code": log_data.get("emp_code"),
-    #                         "emp_name": log_data.get("employee_id")[1],
-    #                         "date": date
-    #                     })
-    #                     number_of_days += 1
-    #     except:
-    #         _logger.warning("Could not retrieve data from client server")
-    #     employee_data = []
-    #     for emp_data in employee_data_dic:
-    #         vals = {
-    #             "partner_id": self.partner_id.id,
-    #             "emp_name": emp_data.get("emp_name"),
-    #             "res_model": "hr.employee",
-    #             "emp_code": emp_data.get("emp_code"),
-    #             "partner_company": emp_data.get("partner_company"),
-    #             "date": emp_data.get("date"),
-    #             "remote_server_id": self.id,
-    #         }
-    #         client_data_rec = self.env["client.data"].search([
-    #             ("date", "=", emp_data.get("date")),
-    #             ("emp_code", "=", emp_data.get("emp_code")),
-    #             ("remote_server_id", "=", self.id),
-    #         ], limit=1)
-    #         if not client_data_rec:
-    #             employee_data.append(vals)
-    #     self.env["client.data"].create(employee_data)
-    #     server["date_sync"] = datetime.now()
-    #     return True
-
     def button_sync_partner_data(self):
         for server in self:
             url = server.url
@@ -186,25 +57,136 @@ class RemoteServer(models.Model):
                 },
             }
 
-    @api.model
-    def _client_log_data_fetch(self):
-        server_datas = self.env["remote.server"].search([])
-        for server in server_datas:
-            server._fetch_client_log_data(server)
-            self._cr.commit()
-        self._generate_employee_creation_charges()
-
-    # @api.model
-    # def _client_log_data_fetch_past_date(self):
-    #     server_datas = self.env["remote.server"].search([])
-    #     for server in server_datas:
-    #         server._fetch_client_past_date_deta(server)
-    #         self._cr.commit()
-    #     self._generate_employee_creation_charges()
-
-    def _fetch_client_one_time_charges_data(self, server):
+    def _fetch_daily_data(self, server):
+        # Get employee active deta base on today date
         log_datas = []
-        employee_data_dic = []
+        if server:
+            addr = server.url
+            userid = server.user
+            password = server.password
+            dbname = server.dbname
+        try:
+            uid = xmlrpclib.ServerProxy("%s/xmlrpc/common" % (addr)).authenticate(
+                dbname, userid, password, {}
+            )
+        except:
+            _logger.warning("Could not authenticate user on the remote server")
+        try:
+            log_datas = xmlrpclib.ServerProxy("%s/xmlrpc/object" % (addr)).execute(
+                dbname,
+                uid,
+                password,
+                "partner.work.history",
+                "search_read",
+                ([
+                    ('employee_id', '!=', False),
+                    '|',
+                    ('end_date', '>=', fields.Date.today()),
+                    ('end_date', '=', False)
+                ]),
+                ["partner_company_id", "employee_id", "emp_code"],
+            )
+        except:
+            _logger.warning("Could not retrieve data from client server")
+        employee_data = []
+        client_data_rec = self.env["client.data"].search([
+            ("date", "=", fields.Date.today()),
+            ("remote_server_id", "=", server.id),
+            ("is_one_time_charges", "=", False),
+        ])
+        for logdata in log_datas:
+            vals = {
+                "partner_id": server.partner_id.id,
+                "emp_name": logdata.get("employee_id")[1],
+                "res_model": "hr.employee",
+                "emp_code": logdata.get("emp_code"),
+                "partner_company": logdata.get("partner_company_id")[1]
+                if logdata.get("partner_company_id")
+                else "",
+                "date": fields.Date.today(),
+                "remote_server_id": server.id,
+            }
+            client_data_ids = client_data_rec.filtered(
+                lambda line: line.emp_code == logdata.get("emp_code"))
+            if not client_data_ids:
+                employee_data.append(vals)
+        self.env["client.data"].create(employee_data)
+        server["date_sync"] = datetime.now()
+        return True
+
+    def _fetch_past_date_deta(self, server):
+        # Get employee past deta data.
+        log_datas = []
+        previous_day_date = fields.Date.today() - relativedelta(days=1)
+        if server:
+            addr = server.url
+            userid = server.user
+            password = server.password
+            dbname = server.dbname
+        try:
+            uid = xmlrpclib.ServerProxy("%s/xmlrpc/common" % (addr)).authenticate(
+                dbname, userid, password, {}
+            )
+        except:
+            _logger.warning("Could not authenticate user on the remote server")
+        try:
+            log_datas = xmlrpclib.ServerProxy("%s/xmlrpc/object" % (addr)).execute(
+                dbname,
+                uid,
+                password,
+                "partner.work.history",
+                "search_read",
+                ([
+                    ('employee_id', '!=', False),
+                    ('join_date', '>=', server.start_date),
+                    ('write_date', '<=', previous_day_date),
+                    ('write_date', '>=', previous_day_date),
+                ]),
+                ["partner_company_id", "employee_id", "emp_code", "join_date", "end_date"],
+            )
+        except:
+            _logger.warning("Could not retrieve data from client server")
+        employee_data = []
+        client_data_rec = self.env["client.data"].search([
+            ("remote_server_id", "=", server.id),
+            ("is_one_time_charges", "=", False),
+        ])
+        for logdata in log_datas:
+            emp_join_date_str = logdata.get("join_date")
+            emp_end_date_str = logdata.get("end_date")
+            emp_end_date = False
+            if emp_end_date_str:
+                emp_end_date = datetime.strptime(emp_end_date_str, "%Y-%m-%d").date()
+            if not emp_end_date:
+                emp_end_date = previous_day_date
+            emp_join_date = datetime.strptime(emp_join_date_str, "%Y-%m-%d").date()
+            differece_days = emp_end_date - emp_join_date
+            number_of_days = 0
+            for day in range(differece_days.days + 1):
+                date = emp_join_date + timedelta(days=number_of_days)
+                vals = {
+                    "partner_id": server.partner_id.id,
+                    "emp_name": logdata.get("employee_id")[1],
+                    "res_model": "hr.employee",
+                    "emp_code": logdata.get("emp_code"),
+                    "partner_company": logdata.get("partner_company_id")[1]
+                    if logdata.get("partner_company_id")
+                    else "",
+                    "date": date,
+                    "remote_server_id": server.id,
+                }
+                number_of_days += 1
+                client_data_ids = client_data_rec.filtered(
+                    lambda line: line.emp_code == logdata.get("emp_code") and line.date == date)
+                if not client_data_ids:
+                    employee_data.append(vals)
+        self.env["client.data"].create(employee_data)
+        server["date_sync"] = datetime.now()
+        return True
+
+    def _fetch_one_time_charges_data(self, server):
+        # Get employee one time charges data base on differece days in conf.
+        log_datas = []
         if server:
             addr = server.url
             userid = server.user
@@ -233,6 +215,9 @@ class RemoteServer(models.Model):
             _logger.warning("Could not retrieve data from client server")
         dict_res = {}
         employee_data = []
+        client_data_rec = self.env["client.data"].search([
+            ("remote_server_id", "=", server.id)
+        ])
         for logdata in log_datas:
             emp_join_date_str = logdata.get("join_date")
             emp_end_date_str = logdata.get("end_date")
@@ -249,7 +234,7 @@ class RemoteServer(models.Model):
                         "emp_code": logdata.get("emp_code")
                     }
                     vals = {
-                        "partner_id": self.partner_id.id,
+                        "partner_id": server.partner_id.id,
                         "emp_name": logdata.get("employee_id")[1],
                         "res_model": "hr.employee",
                         "emp_code": logdata.get("emp_code"),
@@ -257,15 +242,12 @@ class RemoteServer(models.Model):
                         if logdata.get("partner_company_id")
                         else "",
                         "date": fields.Date.today(),
-                        "remote_server_id": self.id,
+                        "remote_server_id": server.id,
                         "is_one_time_charges": True,
                     }
-                    client_data_rec = self.env["client.data"].search([
-                        ("emp_code", "=", logdata.get("emp_code")),
-                        ("remote_server_id", "=", self.id),
-                        ("is_one_time_charges", "=", True),
-                    ], limit=1)
-                    if not client_data_rec:
+                    client_data_ids = client_data_rec.filtered(
+                        lambda line: line.emp_code == logdata.get("emp_code") and line.is_one_time_charges)
+                    if not client_data_ids:
                         employee_data.append(vals)
             elif differece_days.days < server.partner_id.days_one_time_charge:
                 total_days = fields.Date.today() - emp_join_date
@@ -274,7 +256,7 @@ class RemoteServer(models.Model):
                     date = emp_join_date + timedelta(days=number_of_days)
                     if emp_end_date >= date:
                         vals = {
-                            "partner_id": self.partner_id.id,
+                            "partner_id": server.partner_id.id,
                             "emp_name": logdata.get("employee_id")[1],
                             "res_model": "hr.employee",
                             "emp_code": logdata.get("emp_code"),
@@ -282,25 +264,38 @@ class RemoteServer(models.Model):
                             if logdata.get("partner_company_id")
                             else "",
                             "date": date,
-                            "remote_server_id": self.id,
+                            "remote_server_id": server.id,
                         }
                         number_of_days += 1
-                        client_data_rec = self.env["client.data"].search([
-                            ("emp_code", "=", logdata.get("emp_code")),
-                            ("remote_server_id", "=", self.id),
-                            ("date", "=", date),
-                        ], limit=1)
-                        if not client_data_rec:
+                        client_data_ids = client_data_rec.filtered(
+                            lambda line: line.emp_code == logdata.get("emp_code") and line.date == date)
+                        if not client_data_ids:
                             employee_data.append(vals)
         self.env["client.data"].create(employee_data)
         server["date_sync"] = datetime.now()
         return True
 
     @api.model
-    def _client_one_time_charges_data(self):
+    def _fetch_employee_daily_data(self):
         server_datas = self.env["remote.server"].search([])
         for server in server_datas:
-            server._fetch_client_one_time_charges_data(server)
+            server._fetch_daily_data(server)
+            self._cr.commit()
+        self._generate_employee_creation_charges()
+
+    @api.model
+    def _fetch_employee_past_date_data(self):
+        server_datas = self.env["remote.server"].search([])
+        for server in server_datas:
+            server._fetch_past_date_deta(server)
+            self._cr.commit()
+        self._generate_employee_creation_charges()
+
+    @api.model
+    def _fetch_one_time_charges_employee_data(self):
+        server_datas = self.env["remote.server"].search([])
+        for server in server_datas:
+            server._fetch_one_time_charges_data(server)
         self._generate_employee_creation_charges()
 
     def _generate_employee_creation_charges(self):
@@ -351,52 +346,3 @@ class RemoteServer(models.Model):
                 'balance_history_id': new_balance_rec.id,
                 'is_paid': True
             })
-
-    # @api.model
-    # def _generate_customer_bill(self):
-    #     previous_month_first_date = fields.Date.today() + relativedelta(
-    #         months=-1, day=1
-    #     )
-    #     previous_month_last_date = previous_month_first_date + relativedelta(
-    #         day=calendar.monthrange(
-    #             previous_month_first_date.year, previous_month_first_date.month
-    #         )[1]
-    #     )
-    #     client_datas = (
-    #         self.env["client.data"]
-    #         .with_context(active_test=False)
-    #         .search(
-    #             [
-    #                 "|",
-    #                 ("date", "=", False),
-    #                 "&",
-    #                 ("date", ">=", previous_month_first_date),
-    #                 ("date", "<=", previous_month_last_date),
-    #             ]
-    #         )
-    #     )
-    #     product_id = self.env.ref(
-    #         "sync_clients_data_master.employee_creation_invoice_product"
-    #     )
-    #     for partner, lines in groupby(client_datas, lambda l: l.partner_id):
-    #         for remote_server, lines in groupby(
-    #             client_datas.filtered(lambda l: l.partner_id.id == partner.id),
-    #             lambda l: l.remote_server_id,
-    #         ):
-    #             invoice_vals = {
-    #                 "partner_id": partner.id,
-    #                 "move_type": "out_invoice",
-    #                 "invoice_line_ids": [
-    #                     (
-    #                         0,
-    #                         0,
-    #                         {
-    #                             "product_id": product_id.id,
-    #                             "quantity": len(list(lines)),
-    #                             "price_unit": remote_server.rate or 0.0,
-    #                             "remote_server_id": remote_server.id,
-    #                         },
-    #                     )
-    #                 ],
-    #             }
-    #             self.env["account.move"].create(invoice_vals).action_post()
