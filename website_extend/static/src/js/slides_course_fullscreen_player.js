9d485aa844f881ce51cb31ea708cea7b32b71569
Bhavesh Patil <bhaveshpatil4996@gmail.com>
2023-08-09 18:44:51 +0530

    [IMP]website_extend: Improvement in Addtional Resources.

--- website_extend/static/src/js/slides_course_fullscreen_player.js
+++ website_extend/static/src/js/slides_course_fullscreen_player.js
@@ -9,9 +9,12 @@ odoo.define('website_extend.fullscreen', function (require) {
         return _.find(slideList, slideMatch);
     };
 
+
+
+
     Fullscreen.include({
-        init: function (parent, slides, defaultSlideId, channelData){
-            var result = this._super.apply(this,arguments);
+        init: function (parent, slides, defaultSlideId, channelData) {
+            var result = this._super.apply(this, arguments);
             this.sidebar._onClickTab = this._onClickTab;
             this.sidebar.events['click .o_wslides_fs_slide_link'] = this._onClickAdditionalResources;
             return result;
@@ -24,19 +27,18 @@ odoo.define('website_extend.fullscreen', function (require) {
             if ($elem.data('canAccess') === 'True') {
                 var isQuiz = $elem.data('isQuiz');
                 var slideID = parseInt($elem.data('id'));
-                var slide = findSlide(this.slideEntries, {id: slideID, isQuiz: isQuiz});
+                var slide = findSlide(this.slideEntries, { id: slideID, isQuiz: isQuiz });
                 this.set('slideEntry', slide);
             }
         },
         _onClickAdditionalResources: function (ev) {
-            ev.stopPropagation();
+            ev.preventDefault();
             var additional_url = $(ev.currentTarget).data('additional_url');
-            $("#additional_resources_iframe").attr({'src': additional_url});
+            $("#additional_resources_iframe").attr({ 'src': additional_url });
             $("#additional_resources_iframe").removeClass('d-none');
             $(".parent_slide").hide();
         }
     });
 
 });
-
 
