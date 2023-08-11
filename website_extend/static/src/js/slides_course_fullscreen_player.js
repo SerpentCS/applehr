b9fb57cd3d0c9603f05600eb5355da097319a1d5
Parvez Qureshi <parvez.qureshi@serpentcs.com>
2023-08-11 11:49:41 +0530

    [IMP]Disable developer key

--- website_extend/static/src/js/slides_course_fullscreen_player.js
+++ website_extend/static/src/js/slides_course_fullscreen_player.js
@@ -18,6 +18,20 @@ odoo.define('website_extend.fullscreen', function (require) {
             document.addEventListener("contextmenu", (event) => {
                 event.preventDefault();
             });
+            function ctrlShiftKey(e, keyCode) {
+                return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+            }
+            document.onkeydown = (e) => {
+                // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+                if (
+                  event.keyCode === 123 ||
+                  ctrlShiftKey(e, 'I') ||
+                  ctrlShiftKey(e, 'J') ||
+                  ctrlShiftKey(e, 'C') ||
+                  (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+                )
+                return false;
+            };
             return result;
         },
         _onClickTab: function (ev) {
